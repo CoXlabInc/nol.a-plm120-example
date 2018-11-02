d805d03e6b9faa5d71b0f0751115e8e474079b88
Jongsoo Jeong <jsjeong@coxlab.kr>
2018-11-02 10:15:09 +0900

    CS-301 Added the first example.

---
+++ Basic/main.cpp
@@ -0,0 +1,119 @@
+// -*- mode:c++; indent-tabs-mode:nil; -*-
+
+#include <cox.h>
+
+Timer ledTimer;
+Timer printTimer;
+
+static void ledOffTask(void *args);
+
+static void ledOnTask(void *args) {
+  ledTimer.onFired(ledOffTask, NULL);
+  ledTimer.startOneShot(10);
+  digitalWrite(GPIO1, HIGH);
+  digitalToggle(GPIO2);
+}
+
+static void ledOffTask(void *args) {
+  ledTimer.onFired(ledOnTask, NULL);
+  ledTimer.startOneShot(990);
+  digitalWrite(GPIO1, LOW);
+}
+
+static const char *weekday[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+static void printTask(void *args) {
+  Serial.write("0123456789\r\n");
+  printf("[%lu usec] Timer works!\n", micros());
+
+  struct tm t;
+  System.getDateTime(t);
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  printf(
+    "Now: %u-%u-%u %s %02u:%02u:%02u (%ld.%06ld)\n",
+    t.tm_year + 1900,
+    t.tm_mon + 1,
+    t.tm_mday,
+    weekday[t.tm_wday],
+    t.tm_hour,
+    t.tm_min,
+    t.tm_sec,
+    now.tv_sec,
+    now.tv_usec
+  );
+
+  printf("Supply voltage: %ld mV\n", System.getSupplyVoltage());
+
+  Serial2.write("0123456789\r\n");
+
+}
+
+static void eventDateTimeAlarm() {
+  struct tm t;
+  System.getDateTime(t);
+  printf("* Alarm! Now: %u-%u-%u %s %02u:%02u:%02u\n", t.tm_year, t.tm_mon, t.tm_mday, weekday[t.tm_wday], t.tm_hour, t.tm_min, t.tm_sec);
+}
+
+static void buttonPressed() {
+  // digitalToggle(GPIO1);
+  printf("[%lu usec] Button works!\n", micros());
+
+  // if (Serial.isBegan()) {
+  //   printf("* Serial is turned off.\n");
+  //   Serial.end();
+  // } else {
+  //   Serial.begin(115200);
+  //   printf("* Serial is turned on.\n");
+  // }
+  // digitalToggle(GPIO1);
+}
+
+static void eventSerialRx(SerialPort &p) {
+  while (p.available() > 0) {
+    char c = p.read();
+    if (c == 'q') {
+      reboot();
+    } else {
+      p.write(c);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.onReceive(eventSerialRx);
+  Serial.listen();
+
+  Serial2.begin(9600);
+  Serial2.onReceive(eventSerialRx);
+  Serial2.listen();
+
+  printf("\n*** [PLM120] Basic Functions ***\n");
+
+  System.setTimeDiff(9 * 60); //KST
+  struct tm t;
+  t.tm_year = 2017 - 1900;
+  t.tm_mon = 8 - 1;
+  t.tm_mday = 24;
+  t.tm_hour = 18;
+  t.tm_min = 37;
+  t.tm_sec = 50;
+  System.setDateTime(t);
+  System.onDateTimeAlarm(eventDateTimeAlarm);
+
+  System.setTimeAlarm(18, 38);
+
+  ledTimer.onFired(ledOffTask, NULL);
+  ledTimer.startOneShot(1000);
+
+  printTimer.onFired(printTask, NULL);
+  printTimer.startPeriodic(1000);
+
+  pinMode(GPIO5, INPUT_PULLUP);
+  attachInterrupt(GPIO5, buttonPressed, FALLING);
+
+  pinMode(GPIO1, OUTPUT);
+  digitalWrite(GPIO1, HIGH);
+  pinMode(GPIO2, OUTPUT);
+  digitalWrite(GPIO2, HIGH);
+}
